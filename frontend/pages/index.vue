3f1f944d584c4d2b485ed8208d1816f812113e82
gleb89 <glebhleb89@icloud.com>
2021-09-15 12:37:52 +0300

    55

---
+++ frontend/pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      {{listproducts}}
+      <v-btn @click="onPage()">66666</v-btn>
+    </v-col>
+  </v-row>
+</template>
+<script>
+export default {
+  asyncData({ $axios, route, error }) {
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    return $axios
+      .$get(`present/`, {
+        headers: headers,
+      })
+      .then((presents) => {
+        return { presents };
+      });
+  },
+  computed: {
+    listproducts() {
+      
+      return this.presents;
+    },
+    methods: {
+      monPage() {
+        this.$router.push('/666')
+      },
+    },
+  }
+}
+</script>
