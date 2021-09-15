f0119036fba6d6939449fb097c7e152e59750707
gleb89 <glebhleb89@icloud.com>
2021-09-15 12:25:38 +0300

    zz

--- frontend/.nuxt/nuxt-vuex-localstorage.js
+++
@@ -1,6 +0,0 @@
-import bindStorage from 'nuxt-vuex-localstorage/plugins/bindStorage'
-
-export default (ctx) => {
-  const options = {"mode":"cookie","expireHours":24}
-  bindStorage(ctx, options)
-}
--- frontend/layouts/loginadmin.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <v-app id="inspire">
-    <v-main>
-      <Nuxt />
-    </v-main>
-  </v-app>
-</template>
