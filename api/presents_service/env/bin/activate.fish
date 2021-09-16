a9ed7c278d891debd6879d719df93f8f3a5fba46
gleb89 <glebhleb89@icloud.com>
2021-09-16 23:31:45 +0300

    test

--- api/presents_service/env/bin/activate.fish
+++ api/presents_service/env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/gleb/projects/presents-service/api/presents_service/env"
+set -gx VIRTUAL_ENV "/home/gleb/projects/chingiz/api/presents_service/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
