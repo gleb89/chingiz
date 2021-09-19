5cf17670b38ed3259bacdad9f77b6bf81566f8a7
gleb89 <glebhleb89@icloud.com>
2021-09-19 04:11:38 +0300

    vv

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -677,13 +677,13 @@ h2{
   text-align: center;
 }
   .box-header .theme--light.v-input{
-    width: 90%;
-    max-width: 90%;
+    width: 100%;
+    max-width: 100%;
     
 
   }
   .box-header .v-text-field.v-text-field--solo .v-input__control {
-  max-width: 90%;
+  max-width: 100%;
   margin-left: 1rem;
   
   }
