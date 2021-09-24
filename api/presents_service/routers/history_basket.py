05448e22ffc759f5415b0c4661dab114bb8f4656
gleb89 <glebhleb89@icloud.com>
2021-09-25 01:10:14 +0300

    44

--- api/presents_service/routers/history_basket.py
+++ api/presents_service/routers/history_basket.py
@@ -21,10 +21,10 @@ async def get_all():
 @history_router.get('/{basket_id}')
 async def get_one_history(basket_id:int):
     history = await HistoryBasket.objects.filter(basket_id=basket_id).all()
-    # try:
-    return history[0].history
-    # except:
-    #     return history
+    if len(history)> 0:
+        return history[0].history
+    else:
+         return history
     
 @history_router.delete('/{basket_id}')
 async def get_one(basket_id:int):
