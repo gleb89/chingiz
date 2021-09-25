c44370ad6d51d50cdfc083807dc8bd8a8aa4b070
gleb89 <glebhleb89@icloud.com>
2021-09-25 12:43:13 +0300

    sdsd

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -61,9 +61,7 @@ export default {
     },
   },
   axios: {
-    // baseURL: 'http://82.148.17.12:8080/api/v1/'
-    // baseURL:"http://localhost:8000/api/v1/"
-    // baseURL:"http://localhost:8080/api/v1/"
+ 
     },
 
   // recaptcha: {
