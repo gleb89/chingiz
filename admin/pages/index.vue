63ef0ec2ab068df16ad0073ea34adda1ef9d7d04
gleb89 <glebhleb89@icloud.com>
2021-10-04 00:29:03 +0300

    gg

--- admin/pages/index.vue
+++ admin/pages/index.vue
@@ -1,17 +1,50 @@
 <template>
-  <div>
-    hello
-  </div>
+    <v-container>
+        <div class="d-flex justify-center">
+            <v-card class="login-form">
+                <FormLoginAdmin/>
+            </v-card>
+        </div>
+         
+    </v-container>
+    
+    
 </template>
 
 <script>
 export default {
-  mounted() {
-    setTimeout(() => {
-      this.$router.push('/admin')
-    }, 500);
-  },
-  
+    layout:'loginadmin',
+    computed: {
+
+    
+    },
+    mounted: function () {
+    if (!this.$store.state.localStorage.jwtToken.length) {
+        console.log(55);
+      this.$router.push('/admin/list_presents')
+    }
+    if (this.$store.state.localStorage.jwtToken.length) {
+        console.log(88);
+        this.$router.push('/admin/list_presents')
+      }
+    
+    },
 }
 </script>
+
+<style >
+.login-form{
+    display: flex;
+    justify-content: center;
+    width: max-content;
+    padding: 1rem;
+}
+@media (max-width: 500px){
+   .login-form{
+    padding: auto;
+    width: 100%;
+  
+} 
+}
+</style>
 
