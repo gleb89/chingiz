897fff620701d1e54a0eace405d719b1296b4aea
gleb89 <glebhleb89@icloud.com>
2021-10-04 00:29:29 +0300

    v

--- admin/pages/admin/index.vue
+++ admin/pages/admin/index.vue
@@ -3,3 +3,19 @@
         
     </div>
 </template>
+
+<script>
+export default {
+        mounted: function () {
+    if (!this.$store.state.localStorage.jwtToken.length) {
+        console.log(55);
+      this.$router.push('/admin/list_presents')
+    }
+    if (this.$store.state.localStorage.jwtToken.length) {
+        console.log(88);
+        this.$router.push('/admin/list_presents')
+      }
+    
+    },
+}
+</script>
