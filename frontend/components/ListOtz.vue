8f5eb70a2dc34c401978f97c825b191af89c7231
gleb89 <glebhleb89@icloud.com>
2021-10-04 20:35:47 +0300

    dfd

--- frontend/components/ListOtz.vue
+++ frontend/components/ListOtz.vue
@@ -34,7 +34,15 @@
 </template>
 
 <style scoped>
-.v-window__next, .v-window__prev {
+.v-window__next {
+    background: rgba(0,0,0,.3);
+    border-radius: 50%;
+    position: absolute;
+    margin: 0 16px;
+    top: 30%;
+    z-index: 1;
+}
+.v-window__prev {
     background: rgba(0,0,0,.3);
     border-radius: 50%;
     position: absolute;
