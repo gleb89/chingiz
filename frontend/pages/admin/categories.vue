bb9d8babde7e330a90771b7e2c5ae91ec4b3bdb5
gleb89 <glebhleb89@icloud.com>
2021-10-06 15:08:37 +0300

    gfg

--- frontend/pages/admin/categories.vue
+++ frontend/pages/admin/categories.vue
@@ -149,7 +149,7 @@
           <img  style="width: 10rem;" :src="item.icon" alt="none">
 
         </template>
-      <!-- изменить удалить -->
+      <!-- изменить удалитggь -->
       <template v-slot:item.actions="{ item }">
         <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
         <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
