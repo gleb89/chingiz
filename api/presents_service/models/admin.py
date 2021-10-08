1a63b6c5758045f55c21e8124e5c0cccf562bba4
gleb89 <glebhleb89@icloud.com>
2021-10-08 18:13:32 +0300

    сс

--- api/presents_service/models/admin.py
+++ api/presents_service/models/admin.py
@@ -25,6 +25,22 @@ class Admin(ormar.Model):
     id: int = ormar.Integer(primary_key=True)
     name: str = ormar.String(max_length=1000)
     password:str = ormar.String(max_length=1000)
+    superuser:bool = ormar.Boolean(nullable=True,default=False)
+    history_basket_read:bool = ormar.Boolean(nullable=True,default=False)
+    present_read:bool = ormar.Boolean(nullable=True,default=False)
+    filters_present_read:bool = ormar.Boolean(nullable=True,default=False)
+    users_read:bool = ormar.Boolean(nullable=True,default=False)
+    comments_read:bool = ormar.Boolean(nullable=True,default=False)
+    podpiska_read:bool = ormar.Boolean(nullable=True,default=False)
+
+    courer_change:bool = ormar.Boolean(nullable=True,default=False)
+    history_basket_change:bool = ormar.Boolean(nullable=True,default=False)
+    present_change:bool = ormar.Boolean(nullable=True,default=False)
+    filters_present_change:bool = ormar.Boolean(nullable=True,default=False)
+    users_change:bool = ormar.Boolean(nullable=True,default=False)
+    comments_change:bool = ormar.Boolean(nullable=True,default=False)
+    podpiska_change:bool = ormar.Boolean(nullable=True,default=False)
+    courer_change:bool = ormar.Boolean(nullable=True,default=False)
 
     @validator('password')
     def hash_password(cls, pw: str) -> str:
