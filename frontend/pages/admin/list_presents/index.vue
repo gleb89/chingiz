47b2366fcdc828a363ee69948bff192e496b8a39
gleb89 <glebhleb89@icloud.com>
2021-10-16 18:35:37 +0300

    gffg

--- frontend/pages/admin/list_presents/index.vue
+++ frontend/pages/admin/list_presents/index.vue
@@ -820,3 +820,11 @@ export default {
   }
 };
 </script>
+<style>
+.v-data-table-header {
+  background: rgb(236, 224, 253) none repeat scroll 0% 0%;
+}
+.prep .theme--light.v-icon {
+  color: rgb(243 10 10 / 54%);
+}
+</style>
