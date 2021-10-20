38f85eba16ac7ea9a66622c31c397ac40aec81c6
gleb89 <glebhleb89@icloud.com>
2021-10-20 14:48:05 +0300

    ff

--- api/presents_service/routers/present.py
+++ api/presents_service/routers/present.py
@@ -113,7 +113,7 @@ async def add_image(
 @precent_router.get('/')
 async def get_all():
 
-    presents =  await Present.objects.sorder_by("sort_id_catalog").prefetch_related(
+    presents =  await Present.objects.prefetch_related(
             [
                 "category__subcategory",
                 "form_precent",
