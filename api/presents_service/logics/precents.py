1542b2c491e96cb744e6831fa3f9bf780376233b
gleb89 <glebhleb89@icloud.com>
2021-10-29 19:22:05 +0300

    images

--- api/presents_service/logics/precents.py
+++ api/presents_service/logics/precents.py
@@ -15,13 +15,28 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 # host = 'https://giftcity.kz:8080'
 host = 'https://giftcity.kz'
 
+async def watermark_photo(input_image_path):
+
+    base_image = Image.open(input_image_path)
+    watermark = Image.open('static/images/logo4.png')
+    width, height = base_image.size
+ 
+    transparent = Image.new('RGBA', (width, height), (0,0,0,0))
+    transparent.paste(base_image, (0,0))
+    transparent.paste(watermark, (width -180,height-70), mask=watermark)
+    transparent.save(input_image_path)
+ 
+
 async def image_add(image):
     data = str(datetime.datetime.now()).replace(" ", "")
     image = Image.open(image.file)
     image.save(f'static/images/{data}origin.webp', format = "WebP", lossless = True)
-    image.save(f'static/images/{data}.webp', format = "WebP", quality=70,optimize=True)
+    image.save(f'static/images/{data}.webp', format = "WebP", quality=40,optimize=True)
     dubl = f'{host}/api/v1/present/static/images/{data}.webp'
     origin = f'{host}/api/v1/present/static/images/{data}origin.webp'
+
+    await watermark_photo(f'static/images/{data}.webp')
+    await watermark_photo(f'static/images/{data}origin.webp')
     return origin, dubl
 
 
