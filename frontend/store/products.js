0fdcdfa69cb5c8f1c85bcc612445317a7081d3aa
gleb89 <glebhleb89@icloud.com>
2021-10-30 11:35:20 +0300

    front redis

--- frontend/store/products.js
+++ frontend/store/products.js
@@ -22,7 +22,7 @@ export const state = () => {
             "Content-Type": "application/json"
           };
         const products = await this.$axios
-        .$get(`https://giftcity.kz/api/v1/present/`,{
+        .$get(`https://giftcity.kz/api/v1/present/catalog`,{
           headers: headers
         })
         commit('setproducts',products)
