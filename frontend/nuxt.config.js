70b2d1f9bfced5b4807859ccf6614f66ba45cf37
gleb89 <glebhleb89@icloud.com>
2021-11-05 22:13:05 +0300

    ffff

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -83,32 +83,32 @@ export default {
     // '@nuxtjs/recaptcha',
 
   ],
-  sitemap: {
-    generate: true,
-    hostname: 'giftcity.kz',
-    exclude: [
-      '/admin',
-      '/admin/list_presents',
-      '/admin/all_couries',
-      '/admin/alladmins',
-      '/admin/categories',
-      '/admin/comments',
-      '/admin/commentserv',
-      '/admin/stocks',
-      '/admin/podpiska',
-      '/admin/users',
-      '/admin/povod',
-      '/admin/orders',
-      '/admin/all_couries',
-      '/cabinet',
-      '/cabinet/basket',
-      '/cabinet/bonus',
-      '/cabinet/history',
-      '/cabinet/oplata',
-      '/oferta',
-      '/basket',
-    ]
-  },
+  // sitemap: {
+  //   generate: true,
+  //   hostname: 'https://giftcity.kz',
+  //   exclude: [
+  //     '/admin',
+  //     '/admin/list_presents',
+  //     '/admin/all_couries',
+  //     '/admin/alladmins',
+  //     '/admin/categories',
+  //     '/admin/comments',
+  //     '/admin/commentserv',
+  //     '/admin/stocks',
+  //     '/admin/podpiska',
+  //     '/admin/users',
+  //     '/admin/povod',
+  //     '/admin/orders',
+  //     '/admin/all_couries',
+  //     '/cabinet',
+  //     '/cabinet/basket',
+  //     '/cabinet/bonus',
+  //     '/cabinet/history',
+  //     '/cabinet/oplata',
+  //     '/oferta',
+  //     '/basket',
+  //   ]
+  // },
 
   
   fontawesome:{
