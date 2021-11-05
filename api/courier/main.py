a1543de0cd81742121ee71501ae585102e04a6fc
gleb89 <glebhleb89@icloud.com>
2021-11-05 10:19:12 +0300

    df

--- api/courier/main.py
+++ api/courier/main.py
@@ -34,7 +34,7 @@ app = FastAPI(
 # cors настройки!
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
